46c049c394e5be9288d882ceb8b593c1b032689f
noyonalways <noyonrahman2003@gmail.com>
2026-01-05 17:43:22 +0600

    feat(cli): add transaction type filter to view command
    Add support for filtering transactions by type (income, expense, transfer) when using the view transactions command. This allows users to narrow down results to specific transaction types they're interested in.

--- apps/cli/src/commands/view-transactions-command.ts
+++ apps/cli/src/commands/view-transactions-command.ts
@@ -2,19 +2,30 @@ import { Command } from 'commander';
 import dependencies from '../dependencies';
 import { ViewAllTransactionsByAccountIdUseCase } from '@expn/core/use_cases/view-all-transactions';
 import { withAuth } from '../utils/withAuth';
+import { TransactionFilter } from '@expn/core/repositories/transaction.repository';
 
 export const viewTransactionsCommand = new Command('transactions')
 	.description('View all transactions for a specific account')
 	.argument('<accountId>', 'The ID of the account to view transactions for')
+	.option('-t, --type <type>', 'Filter transactions by type (income, expense, transfer)')
 	.action(
-		withAuth(async ({ userId }, accountId) => {
+		withAuth(async ({ userId }, accountId, options) => {
 			try {
 				const viewTransactions = new ViewAllTransactionsByAccountIdUseCase(
 					dependencies.repo.transactionRepository,
 					dependencies.lib.logger
 				);
 
-				const transactions = await viewTransactions.execute(accountId);
+				const filter: TransactionFilter = {};
+				if (options.type) {
+					if (!['income', 'expense', 'transfer'].includes(options.type)) {
+						console.error('Invalid transaction type. Must be income, expense, or transfer.');
+						return;
+					}
+					filter.type = options.type;
+				}
+
+				const transactions = await viewTransactions.execute(accountId, filter);
 				console.table(
 					transactions.map((t) => ({
 						ID: t.id,
