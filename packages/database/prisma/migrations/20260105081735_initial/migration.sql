ec4408a469c8365d66ec81ea6b3039630084528c
noyonalways <noyonrahman2003@gmail.com>
2026-01-05 14:18:35 +0600

    feat(database): add initial database schema and setup
    - Add initial Prisma schema with users, accounts and transactions tables
    - Configure database package with Prisma and Drizzle ORM dependencies
    - Update docker-compose to use Postgres 16
    - Add VS Code settings for Prisma and spell check

---
+++ packages/database/prisma/migrations/20260105081735_initial/migration.sql
@@ -0,0 +1,47 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('income', 'expense', 'transfer');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "balance" DECIMAL(10,2) NOT NULL,
+    "currency" TEXT NOT NULL,
+    "last_updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" TEXT NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
+    "last_balance" DECIMAL(10,2) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "account_id" TEXT NOT NULL,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_user_id_currency_key" ON "accounts"("user_id", "currency");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
